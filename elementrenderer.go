a6bd79920c40cb96b20db792e4bd45838df1d1a6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-25 15:40:00 -0700

    refactor

--- elementrenderer.go
+++ elementrenderer.go
@@ -4,6 +4,13 @@ import (
 	"io"
 )
 
+// ElementRenderer is a renderer a single type of element.
+//
+// For example, it might just handle rendering a bold element.
+// Or, also for example, it might just handle rendering an italics element.
+// Etc.
+//
+// Typically, one gets an ElementRenderer from Renderer.
 type ElementRenderer interface {
 	RenderRune(io.Writer, rune) error
 	BeginRender(io.Writer) error
