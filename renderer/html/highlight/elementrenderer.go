651466b321b2baaae03bce4f7c4bda02b07daacc
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-26 16:30:10 -0700

    wikiwikihtmlhighlight

---
+++ renderer/html/highlight/elementrenderer.go
@@ -0,0 +1,61 @@
+package wikiwikihtmlhighlight
+
+import (
+	"io"
+
+	"sourcecode.social/reiver/go-erorr"
+
+	"github.com/reiver/go-wikiwiki/renderer"
+	"github.com/reiver/go-wikiwiki/renderer/html/rune"
+)
+
+const elementbegin string = "<mark>"
+const elementend   string = "</mark>"
+const elementname  string = "highlight"
+
+var ElementRenderer wikiwikirenderer.ElementRenderer = internalElementRenderer{}
+
+type internalElementRenderer struct {}
+
+func (internalElementRenderer) RenderRune(writer io.Writer, r rune) error {
+	if nil == writer {
+		return errNilWriter
+	}
+
+	return wikiwikihtmlrune.RenderRune(writer, r)
+}
+
+func (internalElementRenderer) BeginRenderingElement(writer io.Writer) error {
+	if nil == writer {
+		return errNilWriter
+	}
+
+	{
+		const str string = elementbegin
+
+		_, err := io.WriteString(writer, str)
+		if nil != err {
+			return erorr.Errorf("wikiwiki: problem writing %s element beginning (%q): %w", elementname, str, err)
+		}
+	}
+
+	return nil
+}
+
+func (internalElementRenderer) EndRenderingElement(writer io.Writer) error {
+	if nil == writer {
+		return errNilWriter
+	}
+
+
+	{
+		const str string = elementend
+
+		_, err := io.WriteString(writer, str)
+		if nil != err {
+			return erorr.Errorf("wikiwiki: problem writing %s element ending (%q): %w", elementname, str, err)
+		}
+	}
+
+	return nil
+}
---
+++ renderer/html/highlight/errors.go
@@ -0,0 +1,9 @@
+package wikiwikihtmlhighlight
+
+import (
+	"sourcecode.social/reiver/go-erorr"
+)
+
+const (
+	errNilWriter = erorr.Error("wikiwiki: nil writer")
+)
