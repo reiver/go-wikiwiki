8b7ff6a4baa739d30185587fc9ebeff01d70af4b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-26 16:40:44 -0700

    wikiwikihtmlsuperscript

---
+++ renderer/html/superscript/elementrenderer.go
@@ -0,0 +1,61 @@
+package wikiwikihtmlsuperscript
+
+import (
+	"io"
+
+	"sourcecode.social/reiver/go-erorr"
+
+	"github.com/reiver/go-wikiwiki/renderer"
+	"github.com/reiver/go-wikiwiki/renderer/html/rune"
+)
+
+const elementbegin string = "<sup>"
+const elementend   string = "</sup>"
+const elementname  string = "superscript"
+
+var ElementRenderer wikiwikirenderer.ElementRenderer = internalElementRenderer{}
+
+type internalElementRenderer struct {}
+
+func (internalElementRenderer) RenderRune(writer io.Writer, r rune) error {
+	if nil == writer {
+		return errNilWriter
+	}
+
+	return wikiwikihtmlrune.RenderRune(writer, r)
+}
+
+func (internalElementRenderer) BeginRenderingElement(writer io.Writer) error {
+	if nil == writer {
+		return errNilWriter
+	}
+
+	{
+		const str string = elementbegin
+
+		_, err := io.WriteString(writer, str)
+		if nil != err {
+			return erorr.Errorf("wikiwiki: problem writing %s element beginning (%q): %w", elementname, str, err)
+		}
+	}
+
+	return nil
+}
+
+func (internalElementRenderer) EndRenderingElement(writer io.Writer) error {
+	if nil == writer {
+		return errNilWriter
+	}
+
+
+	{
+		const str string = elementend
+
+		_, err := io.WriteString(writer, str)
+		if nil != err {
+			return erorr.Errorf("wikiwiki: problem writing %s element ending (%q): %w", elementname, str, err)
+		}
+	}
+
+	return nil
+}
