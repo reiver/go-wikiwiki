a6f5ad3834393ada96e35e04104abd4469fe0301
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-26 21:25:17 -0700

    wikiwiki.TranscodeToHTML

---
+++ transcode.go
@@ -0,0 +1,59 @@
+package wikiwiki
+
+import (
+	"errors"
+	"io"
+
+	"sourcecode.social/reiver/go-erorr"
+	"sourcecode.social/reiver/go-utf8"
+
+	"github.com/reiver/go-wikiwiki/renderer"
+	"github.com/reiver/go-wikiwiki/renderer/html/text"
+	"github.com/reiver/go-wikiwiki/transcoder"
+	"github.com/reiver/go-wikiwiki/transcoder/text"
+)
+
+func TranscodeToHTML(writer io.Writer, reader io.Reader) (err error) {
+	if nil == writer {
+		return errNilWriter
+	}
+	if nil == reader {
+		return errNilReader
+	}
+
+	var textrenderer wikiwikirenderer.TextRenderer = wikiwikihtmltext.TextRenderer
+	if nil == textrenderer {
+		return errInternalError
+	}
+
+	var texttranscoder wikiwikitranscoder.TextTranscoder = wikiwikitexttranscoder.NewTextTranscoder(writer, textrenderer)
+	if nil == texttranscoder {
+		return errInternalError
+	}
+	defer func() {
+		err = texttranscoder.Close()
+		if nil != err {
+			err = erorr.Errorf("wikiwiki: problem closing text-transcoder: %w", err)
+		}
+	}()
+
+	for {
+		var r rune
+
+		r, _, err = utf8.ReadRune(reader)
+		if errors.Is(err, io.EOF) {
+	/////////////// BREAK
+			break
+		}
+		if nil != err {
+
+		}
+
+		err = texttranscoder.InterpretRune(r)
+		if nil != err {
+			return erorr.Errorf("wikiwiki: text-transcoder had trouble interpretting rune %q (%U): %w", r, r, err)
+		}
+	}
+
+	return nil
+}
---
+++ transcode_example_test.go
@@ -0,0 +1,44 @@
+package wikiwiki_test
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/reiver/go-wikiwiki"
+)
+
+func ExampleTranscode() {
+
+	var wikitext string =
+		"Hello world!"+"\n"+
+		"This is the **second** sentence."+"\n"+
+		"Did you know that: 0 = 1 + e''iπ''."+"\n"+
+		"This is the //fourth// sentence."+"\n"+
+		"This is the ``fifth`` sentence."+"\n"+
+		"Apple ||BANANA|| cherry."+"\n"+
+		"This is the ||7''th''|| sentence."+"\n"+
+		"‾‾x‾‾ = (x,,1,,, x,,2,,, x,,3,,)"+"\n"
+
+	var input io.Reader = strings.NewReader(wikitext)
+
+	var output strings.Builder
+
+	err := wikiwiki.TranscodeToHTML(&output, input)
+	if nil != err {
+		panic(err)
+	}
+
+	fmt.Print(output.String())
+
+	// Output:
+	//
+	// Hello world!
+	// This is the <b>second</b> sentence.
+	// Did you know that: 0 = 1 + e<sup>iπ</sup>.
+	// This is the <i>fourth</i> sentence.
+	// This is the <tt>fifth</tt> sentence.
+	// Apple <mark>BANANA</mark> cherry.
+	// This is the <mark>7<sup>th</sup></mark> sentence.
+	// <span style="text-decoration:overline">x</span> = (x<sub>1</sub>, x<sub>2</sub>, x<sub>3</sub>)
+}
