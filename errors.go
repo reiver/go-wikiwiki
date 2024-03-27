725c041ee566d0ebce0a42650aa1439075ca7fb2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-26 21:24:14 -0700

    refactor

--- errors.go
+++ errors.go
@@ -5,13 +5,10 @@ import (
 )
 
 const (
+	errInternalError     = erorr.Error("wikiwiki: internal error")
 	errNilReader         = erorr.Error("wikiwiki: nil reader")
 	errNilReceiver       = erorr.Error("wikiwiki: nil receiver")
 	errNilRenderer       = erorr.Error("wikiwiki: nil renderer")
 	errNilTextTranscoder = erorr.Error("wikiwiki: nil text-transcoder")
 	errNilWriter         = erorr.Error("wikiwiki: nil writer")
-)
-
-const (
-	ErrEmptyLine = erorr.Error("empty line")
 )
--- renderer/html/errors.go
+++
@@ -1,9 +0,0 @@
-package wikiwikihtml
-
-import (
-	"sourcecode.social/reiver/go-erorr"
-)
-
-const (
-	errNilWriter = erorr.Error("wikiwiki: nil writer")
-)
