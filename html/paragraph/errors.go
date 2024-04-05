7acfa2d97c0ff3c5461b7747d68b0975bf228886
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-04-04 23:59:07 -0700

    refactoring

---
+++ html/paragraph/errors.go
@@ -0,0 +1,10 @@
+package paragraph
+
+import (
+	"sourcecode.social/reiver/go-erorr"
+)
+
+const (
+	errNilReader         = erorr.Error("wikiwiki: nil reader")
+	errNilWriter         = erorr.Error("wikiwiki: nil writer")
+)
