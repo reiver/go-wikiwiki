3169567ccfadeb6ee85a0c819b035f62b5ca02cc
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-17 15:00:10 -0800

    wikiwikihtml.TransCodeToStringError()

--- html/transcodetostring.go
+++ html/transcodetostring.go
@@ -7,12 +7,22 @@ import (
 )
 
 func TransCodeToString(wikitext string) string {
-	var dst strings.Builder
-
-	err := Transcode(&dst, strings.NewReader(wikitext))
+	result, err := TransCodeToStringError(wikitext)
 	if nil != err {
 		return htmlescape.String(wikitext)
 	}
 
-	return dst.String()
+	return result
+}
+
+func TransCodeToStringError(wikitext string) (string, error) {
+	var dst strings.Builder
+
+	err := Transcode(&dst, strings.NewReader(wikitext))
+	if nil != err {
+		var nada string
+		return nada, err
+	}
+
+	return dst.String(), nil
 }
